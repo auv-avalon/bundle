3f19985eb23998b6c8f0287194a3ea06de30107f
Sascha Arnold <sascha.arnold@dfki.de>
2011-05-12 23:06:46 +0200

    Revert "made some properties accessible"
    This reverts commit e10389211cef412fa1931a799c6d36a91c7c5045.

--- tasks/orogen/sonar_detector.rb
+++ tasks/orogen/sonar_detector.rb
@@ -2,11 +2,7 @@ class Sonardetector::Task
 
     def configure
         super
-        sonardetector.enable_beam_threshold = true
-        sonardetector.beam_threshold_min = 0.5
-        sonardetector.beam_threshold_max = 5
-        sonardetector.enable_wall_estimation = true
-        sonardetector.min_response_value = 10
+    orogen_task.use_visualization = false
     end
 end
 
