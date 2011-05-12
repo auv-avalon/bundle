e10389211cef412fa1931a799c6d36a91c7c5045
Sascha Arnold <sascha.arnold@dfki.de>
2011-05-12 23:05:28 +0200

    made some properties accessible

--- tasks/orogen/sonar_detector.rb
+++ tasks/orogen/sonar_detector.rb
@@ -2,7 +2,11 @@ class Sonardetector::Task
 
     def configure
         super
-    orogen_task.use_visualization = false
+        sonardetector.enable_beam_threshold = true
+        sonardetector.beam_threshold_min = 0.5
+        sonardetector.beam_threshold_max = 5
+        sonardetector.enable_wall_estimation = true
+        sonardetector.min_response_value = 10
     end
 end
 
