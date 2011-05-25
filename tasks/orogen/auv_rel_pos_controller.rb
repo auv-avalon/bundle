bab17a04c10fa889c55f9a677ced90abdc8d526a
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2011-05-25 08:30:59 +0200

    update auv_rel_pos_controller parameter

--- tasks/orogen/auv_rel_pos_controller.rb
+++ tasks/orogen/auv_rel_pos_controller.rb
@@ -5,7 +5,6 @@ class AuvRelPosController::Task
 
     orogen_task.rel_heading = 1
     orogen_task.rel_z = 0
-    #orogen_task.fixed_z = -2.3
     orogen_task.timeout = 30
 
     pid_settings = orogen_task.controller_x
@@ -21,12 +20,11 @@ class AuvRelPosController::Task
     pid_settings = orogen_task.controller_y
     pid_settings.zero!
     pid_settings.Ts = 0.01
-    pid_settings.K = 8
+    pid_settings.K = 6
     pid_settings.Ti = 0.0
-    pid_settings.Td = 1 
-    pid_settings.YMin = -2 #-0.2
-    pid_settings.YMax = 2  #0.2
+    pid_settings.Td = 0.8 
+    pid_settings.YMin = -1 #-0.2
+    pid_settings.YMax = 1  #0.2
     orogen_task.controller_y = pid_settings
     end
-
 end
