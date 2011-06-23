95ec4236a3d027381b6cd14a60d280a9aae16a96
Allan E. Conquest <conquest@informatik.uni-bremen.de>
2011-06-23 18:30:12 +0200

    Added Joystick and started to integrate it into ControlLoop

--- config/deployments/simulation.rb
+++ config/deployments/simulation.rb
@@ -1,8 +1,14 @@
 Roby.app.load_orocos_deployment 'main'
 
-# use_deployments_from 'controldev'
+use_deployments_from 'controldev'
+use_deployments_from 'avalon_back'
 
-# add(Cmp::AUVJoystickCommand)
+add(Cmp::ControlLoop).
+    use(Cmp::AUVJoystickCommand.use(Controldev::Local))
+
+#add(Cmp::AUVJoystickCommand).
+#    use(Controldev::Local)
+
 add_mission(AvalonSimulation::Task)
 
 ### not finished or tested
