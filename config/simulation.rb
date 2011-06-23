d44189353523aa945b6d7d1a5e2fe8b4cb6047df
Chris Mueller <christoph.mueller@dfki.de>
2011-06-23 18:11:11 +0200

    sim: add missing period statement for the camera devices
    They should not be required per se, but the
    instanciation fails without them

--- config/simulation.rb
+++ config/simulation.rb
@@ -4,10 +4,13 @@ Roby.app.use_deployments_from "controldev" # Joystick
 
 Robot.devices do    
     device(Dev::Joystick, :as => 'joystick')
-    device(Dev::Camera, :as => 'bottom_camera', :using => AvalonSimulation::BottomCamera)
-    device(Dev::Camera, :as => 'front_camera', :using => AvalonSimulation::FrontCamera)
+
+    device(Dev::Camera, :as => 'bottom_camera', :using => AvalonSimulation::BottomCamera).
+      period(0.1)
 
     device(Dev::SimulatedMotionActuator)
+    device(Dev::Camera, :as => 'front_camera', :using => AvalonSimulation::FrontCamera).
+      period(0.1)
 end
 
 ### not finished or tested
