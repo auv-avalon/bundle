d6f41226b70a4b578949cc8f269552e32558ae98
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-06-27 11:56:19 +0200

    add an example of the scripting interface

--- planners/main.rb
+++ planners/main.rb
@@ -1,5 +1,81 @@
 # The main planner. A planner of this model is automatically added in the
 # Interface planner list.
 class MainPlanner < Roby::Planning::Planner
+    describe("moves forward and turns on pipeline following if a pipeline is detected").
+        required_arg("z", "the Z value at which we should search for the pipeline").
+        required_arg("speed", "the forward speed at which we should search for the pipeline")
+
+    GATE_TURN_DIRECTION = 1
+
+    method(:find_and_follow_pipeline) do
+        z     = arguments[:z]
+        speed = arguments[:speed]
+
+        # Get a task representing the define('pipeline')
+        pipeline = self.pipeline
+
+        # Code the actual actions
+        pipeline.script do
+            setup_logger(Robot)
+
+            # Define a 'orientation_reader' and 'orientation' methods that allow
+            # access to control.pose.orientation_z_samples
+            data_reader 'orientation', ['control', 'orientation_with_z', 'orientation_z_samples']
+            data_reader 'pipeline_servoing_command', ['detector', 'relative_position_command']
+
+            # Define 'motion_command_writer', 'motion_command' and 'write_motion_command'
+            data_writer 'motion_command', ['control', 'controller', 'command']
+            data_writer 'rel_pos_command', ['control', 'command', 'position_command']
+
+            # <blabla>_child
+            #    returns the child named 'blabla' from the receiver
+            #
+            #    i.e. control_child => returns the Cmp::ControlLoop
+            #         control_child.command_child => returns  the AUVMotionController
+            #
+            # <blabla>_port
+            #    returns the port named 'blabla' from the receiver
+            #         control_child.command_child.motion_command_port => returns motion_command_port from the AUVMotionController
+
+            execute do
+                control_child.command_child.motion_command_port.disconnect_from \
+                    control_child.controller_child.command_port
+            end
+            poll do
+                if orientation
+                    motion_command.heading = orientation.orientation.yaw
+                    transition!
+                end
+            end
+            # Go forward until the component reports FOLLOW_PIPE
+            poll_until detector_child.follow_pipe_event do
+                motion_command.z = z
+                motion_command.x_speed = speed
+                motion_command.y_speed = 0
+                write_motion_command
+            end
+
+            emit :success
+        end
+    end
 end
 
+# Other operations
+#
+#     wait detector_child.follow_pipe_event
+#
+#     poll do
+#        if <condition>
+#          transition!
+#        end
+#     end
+#
+#     poll do
+#       if <condition>
+#         # delayed transition
+#         transition!(20)
+#       end
+#     end
+#
+#     emit <event>
+#       emits the event on the task
