faee87d216d31b068708a305054a5bafe0e218b3
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-06-28 17:34:44 +0200

    small style fix

--- config/deployments/avalon.rb
+++ config/deployments/avalon.rb
@@ -1,11 +1,11 @@
 Roby.app.load_orocos_deployment 'main'
 
-use Srv::Orientation => Cmp::OrientationEstimator
+use Srv::Orientation      => Cmp::OrientationEstimator
 use Srv::OrientationWithZ => Cmp::OrientationEstimator
-use Srv::Pose        => Cmp::PoseEstimator
+use Srv::Pose             => Cmp::PoseEstimator
 
 use Cmp::OrientationEstimator => Cmp::OrientationEstimator.use('low_level_board')
-use Cmp::PoseEstimator => Cmp::PoseEstimator.use('sonar', Cmp::OrientationEstimator)
+use Cmp::PoseEstimator        => Cmp::PoseEstimator.use('sonar', Cmp::OrientationEstimator)
 
 define('pose_estimator', Cmp::PoseEstimator)
 define('orientation_estimator', Cmp::OrientationEstimator)
