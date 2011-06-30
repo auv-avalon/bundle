8563f3aca351827cd55ac006eb8c1091933ff2c9
avalon-rear <avalon@rearPC>
2011-06-30 15:25:55 +0200

    cleanup avalon's controller file

--- controllers/avalon.rb
+++ controllers/avalon.rb
@@ -6,157 +6,33 @@ Roby.app.apply_orocos_deployment 'avalon' # load config/deployments/avalon.rb
 
 navigation_mode = nil 
 
-#Orocos.log_all_ports :exclude_types => '/can/Message'#, :exclude_ports => 'can.hbridge_set'
-#Orocos.log_all_ports :exclude_types => '/canbus/Message'#, :exclude_ports => 'can.hbridge_set'
-
-#Roby.every(1, :on_error => :disable) do
-#	task = Roby.plan.find_tasks(Orocos::RobyPlugin::Dynamixel::Task).running.find { true }
-#	if(task)
-#		writer = task.cmd_angle.writer
-#		writer.write(1)
-#	end
-#end
-
-
-
-Roby.every(0.1, :on_error => :disable) do
-#    State.lowlevel_substate = 7
-    if State.lowlevel_state?
-        if State.lowlevel_state != 3 and State.lowlevel_state != 5
-            if navigation_mode
-                Roby.plan.unmark_mission(navigation_mode.task)
-		navigation_mode = nil
-            end
-        elsif State.lowlevel_state == 3 or State.lowlevel_state == 5
-            if !State.navigation_mode?
-                Robot.warn "switched to mode 3, but no navigation mode is selected in State.navigation_mode"
-            elsif !navigation_mode
-	    	Robot.info "Starting mode number: #{State.lowlevel_substate}"
-                if(State.navigation_mode[State.lowlevel_substate])
-			Robot.info "starting navigation mode #{State.navigation_mode[State.lowlevel_substate]}, we are currently at #{navigation_mode}"
-			navigation_mode, _ = Robot.send("#{State.navigation_mode[State.lowlevel_substate]}!")
-	               	navigation_mode = navigation_mode.as_service
-		elsif
-			Robot.info "Cannot Start unknown substate!!!!! -#{State.navigation_mode[State.lowlevel_substate]}-"
-		end
-            end
-        end
-    end
-end
+# Roby.every(0.1, :on_error => :disable) do
+# #    State.lowlevel_substate = 7
+#     if State.lowlevel_state?
+#         if State.lowlevel_state != 3 and State.lowlevel_state != 5
+#             if navigation_mode
+#                 Roby.plan.unmark_mission(navigation_mode.task)
+# 		navigation_mode = nil
+#             end
+#         elsif State.lowlevel_state == 3 or State.lowlevel_state == 5
+#             if !State.navigation_mode?
+#                 Robot.warn "switched to mode 3, but no navigation mode is selected in State.navigation_mode"
+#             elsif !navigation_mode
+# 	    	Robot.info "Starting mode number: #{State.lowlevel_substate}"
+#                 if(State.navigation_mode[State.lowlevel_substate])
+# 			Robot.info "starting navigation mode #{State.navigation_mode[State.lowlevel_substate]}, we are currently at #{navigation_mode}"
+# 			navigation_mode, _ = Robot.send("#{State.navigation_mode[State.lowlevel_substate]}!")
+# 	               	navigation_mode = navigation_mode.as_service
+# 		elsif
+# 			Robot.info "Cannot Start unknown substate!!!!! -#{State.navigation_mode[State.lowlevel_substate]}-"
+# 		end
+#             end
+#         end
+#     end
+# end
 
 Roby.every(1, :on_error => :disable) do
 	#pp "Current State is:"
 	#a = State.lowlevel_state
 	#pp a 
 end
-
-#    if State.milestone1_mode? && State.milestone1_mode == :from_the_pond
-#        if !State.milestone1?
-#            State.drive_mode = 'trajectories'
-#            State.reverse_trajectory = false
-#            if State.pose?
-#                State.milestone1 = :part_1
-#                Robot.info "Milestone1: went into part 1"
-#            end
-#        end
-#
-#        # Start point : 24, 48
-#        if State.milestone1 == :part_1
-#            # Look for our position
-#            x, y, _ = State.pose.position.data.to_a
-#            if x > 24
-#                # Start mode switching
-#                following = Roby.plan.find_tasks(Orocos::RobyPlugin::TrajectoryController::Task).
-#                    to_a.first
-#                if following
-#                    Orocos.engine.remove(Orocos::DataServices::Driving)
-#                    change_time = Time.now
-#                    State.milestone1 = :part_1_end
-#                    Robot.info "Milestone1: waiting for part 1 to end"
-#                end
-#            end
-#        end
-#
-#        # End point: 28, 50
-#        if State.milestone1 == :part_1_end
-#            if Time.now - change_time > 5
-#                Orocos.engine.add_mission('visual_servoing')
-#                State.milestone1 = :part_2_init
-#            end
-#        end
-#
-#        if State.milestone1 == :part_2_init
-#            # Look for a servoing task
-#            servoing_task = Roby.plan.find_tasks(Orocos::RobyPlugin::VisualServoing::Task).
-#                to_a.first
-#            if servoing_task
-#                servoing_task.on :stop do |event|
-#                    Orocos.engine.remove(Orocos::DataServices::Driving)
-#                    State.milestone1 = :part_2_end
-#                    change_time = Time.now
-#                    Robot.info "Milestone1: waiting for part 2 to end"
-#                end
-#
-#                State.milestone1 = :part_2
-#                Robot.info "Milestone1: in part 2"
-#                change_time = Time.now
-#            end
-#        end
-#
-#        if State.milestone1 == :part_2
-#            if Time.now - change_time > 90
-#                servoing_task = Roby.plan.find_tasks(Orocos::RobyPlugin::VisualServoing::Task).
-#                    to_a.first
-#                if servoing_task.running? && !servoing_task.stop_event.pending?
-#                    servoing_task.stop! 
-#                end
-#            end
-#        end
-#
-#        if State.milestone1 == :part_2_end
-#            if Time.now - change_time > 5
-#                Orocos.engine.add_mission('trajectories')
-#                State.milestone1 = :part_3
-#                Robot.info "Milestone1: in part 3"
-#            end
-#        end
-#    end
-#
-#    if State.milestone1_mode? && State.milestone1_mode == :from_the_bridge
-#        if !State.milestone1?
-#            State.reverse_trajectory = true
-#            State.drive_mode = 'visual_servoing'
-#            if State.pose?
-#                State.milestone1 = :part_1_init
-#                Robot.info "Milestone1: went into part 1"
-#            end
-#        end
-#
-#        if State.milestone1 == :part_1_init
-#            # Look for a servoing task
-#            servoing_task = Roby.plan.find_tasks(Orocos::RobyPlugin::VisualServoing::Task).
-#                to_a.first
-#            if servoing_task
-#                servoing_task.on :stop do |event|
-#                    Orocos.engine.remove(Orocos::DataServices::Driving)
-#                    State.milestone1 = :part_1_end
-#                    change_time = Time.now
-#                    Robot.info "Milestone1: waiting for part 1 to end"
-#                end
-#
-#                State.milestone1 = :part_1
-#                Robot.info "Milestone1: in part 1"
-#                change_time = Time.now
-#            end
-#        end
-#
-#        if State.milestone1 == :part_1_end
-#            # Change back to trajectories only if we got a RTK_FIXED position
-#            if State.pose? && State.pose.position.data[0] > 20 && Time.now - change_time > 5
-#                Orocos.engine.add_mission('trajectories')
-#                State.milestone1 = :part_2
-#                Robot.info "Milestone1: in part 2"
-#            end
-#        end
-#    end
-#end
--- controllers/common_controller.rb
+++ controllers/common_controller.rb
@@ -1,22 +1,26 @@
-# require 'highline'
-# CONSOLE = HighLine.new
-# def color(string, *args)
-#     CONSOLE.color(string, *args)
-# end
-
-def q_to_yaw(q)
-    q0, q1, q2, q3 = q.re, *q.im
-    a = 2 * q1 * q2 + 2 * q0 * q3
-    b = 2 * q0 * q0 + 2 * q1 * q1 - 1
-    Math.atan2(a, b)
+require 'highline'
+CONSOLE = HighLine.new
+def color(string, *args)
+    CONSOLE.color(string, *args)
 end
 
 def add_status(status, name, format, obj, field, *colors)
-    if obj.respond_to?(field)
-        value = obj.send(field)
+    if !field.respond_to?(:to_ary)
+        field = [field]
+    end
+
+    value = field.inject(obj) do |value, field_name|
+        if value.respond_to?(field_name)
+            value.send(field_name)
+        else break
+        end
+    end
+
+    if value
         if block_given?
             value = yield(value)
         end
+
         if value
             if format
                 if !value.respond_to?(:to_ary)
@@ -34,38 +38,16 @@ def add_status(status, name, format, obj, field, *colors)
 end
 
 
-ERR_TIME_DIFF = 0.2
 Roby.every(1, :on_error => :disable) do
     status = []
-    #add_status(status, "needs_external_encoders_calibration", nil, State, :external_encoders_calibrated, :bold, :red) do |s|
-    #    !s
-    #end
 
-    #add_status(status, "time_sync", "%.2f", State.gps, :time, :bold, :red) do |s|
-    #    cpu = s.cpu_time
-    #    gps = s.gps_time
-#   #     diff = (cpu - gps).abs 
-    #    diff = 0
-    #    if diff > ERR_TIME_DIFF
-    #        diff
-    #    end
-    #end
+    add_status(status, "syslevel", "%i", State, :lowlevel_state)
+    add_status(status, "pos", "(x=%.2f y=%.2f z=%.2f)", State, [:pose, :position]) do |p|
+        p.to_a
+    end
+    add_status(status, "heading", "(th=%.2f)", State, [:pose, :orientation]) do |q|
+        q.to_euler(2, 1, 0)[0] * 180.0 / Math::PI
+    end
 
-    #add_status(status, "syslevel", "%i", State, :lowlevel_state)
-    #add_status(status, "pos", "(x=%.2f y=%.2f z=%.2f th=%.2f)", State, :pose) do |p|
-    #    q = p.orientation
-    #    xyzth = p.position.to_a
-    #    xyzth << q.to_euler(2, 1, 0)[0] * 180.0 / Math::PI
-    #end
-    #add_status(status, "gps", "(%s %.2f %.2f %.2f)", State.gps, :solution) do |s|
-    #    [s.positionType.to_s, s.deviationLatitude, s.deviationLongitude, s.deviationAltitude]
-    #end
-    #add_status(status, "gps_pos", "(%.2f %.2f %.2f)", State.gps, :position) do |s|
-    #    [s.position.to_a]
-    #end
-    ##add_status(status, "wheel_pos", "(%.2f %.2f %.2f %.2f)", State, :wheel_pos) do |s|
-    ##      s
-    ##end
-#
-    #Robot.info status.join(' ') if !status.empty?
+    Robot.info status.join(' ') if !status.empty?
 end
