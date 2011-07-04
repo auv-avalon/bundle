0786795928c4ed392a2fb76c8dadb8b74cc27aff
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-07-04 23:05:24 +0200

    implement the sim_set_position(x, y, z) method on the shell in simulation

--- controllers/simulation.rb
+++ controllers/simulation.rb
@@ -1,3 +1,10 @@
 require 'controllers/common_controller'
 Roby.app.apply_orocos_deployment 'simulation' # load config/deployments/avalon.rb
 
+module Robot
+    def self.sim_set_position(x, y, z)
+        task = Orocos::TaskContext.get 'avalon_simulation'
+        task.setPosition(x, y, z)
+    end
+end
+
