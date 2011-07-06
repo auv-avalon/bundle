0aaa704ba7fffb978a6d0a8ce67bb3ed5fc88850
avalon-rear <avalon@rearPC>
2011-07-06 21:48:48 +0200

    renamed and added definitions for different wall following applications

--- config/deployments/main.rb
+++ config/deployments/main.rb
@@ -17,7 +17,7 @@ define('asv_detector', Cmp::AsvDetector.use('left_unicap_camera'))
 
 define('rotation', Cmp::VisualServoing.use(Cmp::Rotation.use('bottom_camera')))
 
-narrow_sonar = device('sonar').use_conf('sonar', 'narrow')
+narrow_sonar = device('sonar').use_conf('sonar', 'narrow_front')
 wall_servoing_right = Cmp::WallDetector.use('sonar').
 	use(narrow_sonar,Sonardetector::Task.use_conf("default","drive-right"))
 wall_servoing_left = Cmp::WallDetector.use('sonar').
@@ -25,4 +25,11 @@ wall_servoing_left = Cmp::WallDetector.use('sonar').
 define('wall_right', Cmp::VisualServoing.use(wall_servoing_right))
 define('wall_left', Cmp::VisualServoing.use(wall_servoing_left))
 define('wall_detector', Cmp::WallDetector.use(narrow_sonar))
+
+
+narrow_right_sonar = device('sonar').use_conf('sonar', 'narrow_right')
+wall_servoing_follow_right = Cmp::WallDetector.
+	use(narrow_right_sonar, Sonardetector::Task.use_conf("default","follow-right-wall"))
+define('wall_follow_right', Cmp::VisualServoing.use(wall_servoing_follow_right))
+define('wall_detector_right', Cmp::WallDetector.use(narrow_right_sonar))
 
