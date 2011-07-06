bd2359721a1141c79f0575cbc0fb3ecf0a5d0db7
Allan E. Conquest <conquest@informatik.uni-bremen.de>
2011-07-06 23:26:01 +0200

    added bottom (rear) sonar to simulation / supervision

--- config/simulation.rb
+++ config/simulation.rb
@@ -36,6 +36,10 @@ Robot.devices do
     device(Dev::MarsSonar, :as => 'sonar', :using => AvalonSimulation::SonarTop).
         period(0.1)
 
+    device(Dev::MarsSonarBottom, :as => 'sonar_rear', :using => AvalonSimulation::SonarBottom).
+        period(0.1)
+
+
     device(Dev::MarsAvalonThrusters, :as => 'thrusters', :using => AvalonSimulation::Actuators).
         period(0.1)
 
