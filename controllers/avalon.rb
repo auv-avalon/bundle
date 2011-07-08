b92b03ebb377c1db55e348fc2e2731e11400ed9a
avalon-rear <avalon@rearPC>
2011-07-08 08:59:00 +0200

    enable mode 5 switching, with soft timeout

--- controllers/avalon.rb
+++ controllers/avalon.rb
@@ -5,34 +5,29 @@ require 'controllers/common_controller'
 Roby.app.apply_orocos_deployment 'avalon' # load config/deployments/avalon.rb
 
 navigation_mode = nil 
+State.navigation_mode = 'wall_left'
 
-# Roby.every(0.1, :on_error => :disable) do
-# #    State.lowlevel_substate = 7
-#     if State.lowlevel_state?
-#         if State.lowlevel_state != 3 and State.lowlevel_state != 5
-#             if navigation_mode
-#                 Roby.plan.unmark_mission(navigation_mode.task)
-# 		navigation_mode = nil
-#             end
-#         elsif State.lowlevel_state == 3 or State.lowlevel_state == 5
-#             if !State.navigation_mode?
-#                 Robot.warn "switched to mode 3, but no navigation mode is selected in State.navigation_mode"
-#             elsif !navigation_mode
-# 	    	Robot.info "Starting mode number: #{State.lowlevel_substate}"
-#                 if(State.navigation_mode[State.lowlevel_substate])
-# 			Robot.info "starting navigation mode #{State.navigation_mode[State.lowlevel_substate]}, we are currently at #{navigation_mode}"
-# 			navigation_mode, _ = Robot.send("#{State.navigation_mode[State.lowlevel_substate]}!")
-# 	               	navigation_mode = navigation_mode.as_service
-# 		elsif
-# 			Robot.info "Cannot Start unknown substate!!!!! -#{State.navigation_mode[State.lowlevel_substate]}-"
-# 		end
-#             end
-#         end
-#     end
-# end
-
-Roby.every(1, :on_error => :disable) do
-	#pp "Current State is:"
-	#a = State.lowlevel_state
-	#pp a 
+run_start = nil
+SOFT_TIMEOUT = 2 * 60
+Roby.every(0.1, :on_error => :disable) do
+    if State.lowlevel_state?
+        if State.lowlevel_state != 5
+            if navigation_mode
+                Roby.plan.unmark_mission(navigation_mode.task)
+		navigation_mode = nil
+            end
+        elsif State.lowlevel_state == 5
+            if !State.navigation_mode?
+                Robot.warn "switched to mode 3, but no navigation mode is selected in State.navigation_mode"
+            elsif !navigation_mode
+		Robot.info "starting #{State.navigation_mode}"
+		navigation_mode, _ = Robot.send("#{State.navigation_mode}!")
+		navigation_mode = navigation_mode.as_service
+		run_start = Time.now
+	    elsif Time.now - run_start > SOFT_TIMEOUT
+	    	Roby.engine.quit
+		Orocos::TaskContext.get('hbridge').stop
+            end
+        end
+    end
 end
