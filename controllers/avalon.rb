95cf7c0addd82ae34743bfc721fec99a72416c5c
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-07-09 11:33:32 +0200

    preset the complete mission on mode 5

--- controllers/avalon.rb
+++ controllers/avalon.rb
@@ -5,10 +5,10 @@ require 'controllers/common_controller'
 Roby.app.apply_orocos_deployment 'avalon' # load config/deployments/avalon.rb
 
 navigation_mode = nil 
-State.navigation_mode = 'wall_left'
+State.navigation_mode = 'autonomous_run'
 
 run_start = nil
-SOFT_TIMEOUT = 2 * 60
+SOFT_TIMEOUT = 15 * 60
 Roby.every(0.1, :on_error => :disable) do
     if State.lowlevel_state?
         if State.lowlevel_state != 5
