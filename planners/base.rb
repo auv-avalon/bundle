b8ccf6309d6a39354d54ba54da945c793d33b930
Allan E. Conquest <conquest@informatik.uni-bremen.de>
2012-02-02 22:50:28 +0100

    fixed some refactoring bugs

--- planners/base.rb
+++ planners/base.rb
@@ -1,5 +1,5 @@
-class MainPlanner
-    describe("alignes to the given yaw and z depth and starts moving forward").
+class MainPlanner < Roby::Planning::Planner
+        describe("alignes to the given yaw and z depth and starts moving forward").
         required_arg("yaw", "initial heading for alignment").
         required_arg("z", "initial z value for alignment").
         required_arg("forward_speed", "forward velocity for motion").
@@ -13,52 +13,57 @@ class MainPlanner
         YAW_THRESHOLD = 10 * Math::PI / 180.0
         Z_THRESHOLD = 0.3
 
-        control = Cmp::ControlLoop.use('command' => AuvRelPosController::Task).as_plan
+        control = Cmp::ControlLoop.use('command' => Orocos::RobyPlugin::AuvRelPosController::Task).as_plan
         control.script do
             data_reader 'orientation', ['orientation_with_z', 'orientation_z_samples']
             data_writer 'motion_command', ['controller', 'command']
 
             wait_any command_child.start_event
 
-            command_child.disconnect_ports(controller_child, [['motion_command', 'command']])
-        end
-
-        # align to the given yaw and z value in this movement
-        poll do
-            motion_command.x_speed = 0
-            motion_command.y_speed = 0
-            motion_command.z = z
-            write_motion_command
-
-            if pose = self.orientation
-                current_yaw = pose.orientation.yaw
-                current_depth = pose.position.z
-
-                yaw_error = normalize_angle(current_yaw - yaw)
-                depth_error = current_depth - z
-
-                transition! if yaw_error.abs < YAW_THRESHOLD and depth_error.abs < Z_THRESHOLD                
-             end
-        end
-
-        # move the aligned auv in respect to z, yaw by given forward_speed and duration
-        if duration and forward_speed > 0.0
-            start_time = nil
             execute do
-                start_time = Time.now
+                command_child.disconnect_ports(controller_child, [['motion_command', 'command']])
             end
 
+                    # align to the given yaw and z value in this movement
             poll do
-                motion_command.x_speed = forward_speed
+                motion_command.x_speed = 0
                 motion_command.y_speed = 0
                 motion_command.z = z
-                motion_command.heading = yaw
                 write_motion_command
 
-                transition! if (Time.now - start_time) > duration
+                if pose = self.orientation
+                    current_yaw = pose.orientation.yaw
+                    current_depth = pose.position.z
+
+                    yaw_error = normalize_angle(current_yaw - yaw)
+                    depth_error = current_depth - z
+
+                    transition! if yaw_error.abs < YAW_THRESHOLD and depth_error.abs < Z_THRESHOLD
+                 end
             end
+
+            # move the aligned auv in respect to z, yaw by given forward_speed and duration
+            if duration and forward_speed > 0.0
+                start_time = nil
+                execute do
+                    start_time = Time.now
+                end
+
+                poll do
+                    motion_command.x_speed = forward_speed
+                    motion_command.y_speed = 0
+                    motion_command.z = z
+                    motion_command.heading = yaw
+                    write_motion_command
+
+                    transition! if (Time.now - start_time) > duration
+                end
+            end
+            emit :success
+
         end
-        emit :success
+
+
     end
 
     # -------------------------------------------------------------------------
--- planners/main.rb
+++ planners/main.rb
@@ -1,18 +1,7 @@
-# Main planner. A planner of this model is automatically added 
+# Main planner. A planner of this model is automatically added
 # to the interface planner list. For Avalon it should provides
 # global utility functions used in plannning scripting.
 class MainPlanner < Roby::Planning::Planner
-
-    # normalize current angle to range between PI and -PI
-    def normalize_angle(angle)
-        if angle < Math::PI
-            angle - 2 * Math::PI
-        elsif angle < -Math::PI
-            angle + 2 * Math::Pi
-        else
-            angle
-        end
-    end
 
     # use a defined data_reader 'orientation_reader' for fetching current yaw
     def get_current_yaw(orientation_reader)
@@ -24,5 +13,18 @@ class MainPlanner < Roby::Planning::Planner
             end
         end
         yaw
+    end
+
+end
+
+
+# normalize current angle to range between PI and -PI
+def normalize_angle(angle)
+    if angle < Math::PI
+        angle - 2 * Math::PI
+    elsif angle < -Math::PI
+        angle + 2 * Math::Pi
+    else
+        angle
     end
 end
