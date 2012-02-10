0379e0a40d77e3029cac3ff168d98cc28d661c57
Chris Mueller <christoph.mueller@dfki.de>
2012-02-10 15:27:50 +0100

    move AUVJoystickCommand composition to tasks/compositionsn/main.rb

--- tasks/compositions/main.rb
+++ tasks/compositions/main.rb
@@ -45,4 +45,13 @@ composition 'VisualServoing' do
     autoconnect
 end
 
+composition 'AUVJoystickCommand' do
+    add Srv::RawCommand, :as => 'rawCommand'
+    add Srv::OrientationWithZ
+    add RawControlCommandConverter::Movement, :as => 'rawCommandConverter'
+    autoconnect
+
+    export rawCommandConverter.motion_command
+    provides Srv::AUVMotionCommand
+end
 
--- tasks/orogen/raw_control_command_converter.rb
+++ tasks/orogen/raw_control_command_converter.rb
@@ -1,10 +1,1 @@
-composition 'AUVJoystickCommand' do
-    add Srv::RawCommand, :as => 'rawCommand'
-    add Srv::OrientationWithZ
-    add RawControlCommandConverter::Movement, :as => 'rawCommandConverter'
-    autoconnect
-
-    export rawCommandConverter.motion_command
-    provides Srv::AUVMotionCommand
-end
 
