8aa5d296df44536a4f552dcc8cc7549d3599d5c2
Chris Mueller <christoph.mueller@dfki.de>
2012-02-11 19:20:44 +0100

    Revert "move AUVJoystickCommand composition to tasks/compositionsn/main.rb"
    This reverts commit 0379e0a40d77e3029cac3ff168d98cc28d661c57.

--- tasks/compositions/main.rb
+++ tasks/compositions/main.rb
@@ -45,13 +45,4 @@ composition 'VisualServoing' do
     autoconnect
 end
 
-composition 'AUVJoystickCommand' do
-    add Srv::RawCommand, :as => 'rawCommand'
-    add Srv::OrientationWithZ
-    add RawControlCommandConverter::Movement, :as => 'rawCommandConverter'
-    autoconnect
-
-    export rawCommandConverter.motion_command
-    provides Srv::AUVMotionCommand
-end
 
--- tasks/orogen/raw_control_command_converter.rb
+++ tasks/orogen/raw_control_command_converter.rb
@@ -1,1 +1,10 @@
+composition 'AUVJoystickCommand' do
+    add Srv::RawCommand, :as => 'rawCommand'
+    add Srv::OrientationWithZ
+    add RawControlCommandConverter::Movement, :as => 'rawCommandConverter'
+    autoconnect
+
+    export rawCommandConverter.motion_command
+    provides Srv::AUVMotionCommand
+end
 
