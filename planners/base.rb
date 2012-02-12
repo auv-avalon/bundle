f02f8a48ad2f72d414d4a83064fbd8653fc53974
Chris Mueller <christoph.mueller@dfki.de>
2012-02-12 22:51:17 +0100

    rename move to align_and_move. add source code for search_frontal_distance.

--- planners/base.rb
+++ planners/base.rb
@@ -4,7 +4,7 @@ class MainPlanner < Roby::Planning::Planner
         required_arg("z", "initial z value for alignment").
         optional_arg("forward_speed", "forward velocity for motion").
         optional_arg("duration", "duration for this forward motion")
-    method(:move) do
+    method(:align_and_move) do
         yaw = arguments[:yaw]
         z = arguments[:z]
         forward_speed = arguments[:forward_speed]
@@ -95,12 +95,63 @@ class MainPlanner < Roby::Planning::Planner
         required_arg("yaw", "initial search direction of this motion method").
         required_arg("z", "initial z value of this search").
         required_arg("forward_speed", "forward velocity for motion").
-        required_arg("distance", "relative distance to a wall in front of avalon").
-        required_arg("angle_range", "scanning range (-angle_range/2, angle_range/2)  in front of avalon").
-        required_arg("timeout", "timeout when this method should be aborted")
-    method(:search_frontal_wall) do
-        # Can use move command for alignment and motion
-        # angle_range = 0 would be a simple beam distance estimator
+        required_arg("distance", "relative distance to a wall in front of avalon in m").
+        optional_arg("timeout", "timeout when this method should be aborted")
+    method(:search_frontal_distance) do
+        yaw = arguments[:yaw]
+        z = arguments[:z]
+        forward_speed = arguments[:forward_speed]
+        distance = arguments[:distance] * 1000
+        timeout = arguments[:timeout]
+
+        WALL_DISTANCE_THRESHOLD = 0.4
+
+        alignment = align_and_move(:yaw => yaw, :z => z)
+
+        sonar_distance = self.sonar_distance
+
+        move_forward = sonar_distance.script do
+            data_reader 'wall_distance', ['laser_scan_provider', 'laserscan']
+            data_writer 'motion_command', ['controller', 'command']
+
+            wait_any command_child.start_event
+
+            execute do
+                # disconnect AuvRelPosController
+                command_child.disconnect_ports(controller_child, [['motion_command', 'command']])
+            end
+
+            poll do 
+                if wall_distance
+                   if wall_distance.minRange > distance
+                        emit :failure
+                   else
+                        transition!
+                   end
+               end
+            end
+
+            poll do
+                motion_command.heading = yaw
+                motion_command.z = z
+                motion_command.y_speed = 0
+                motion_command.x_speed = forward_speed
+                write_motion_command
+
+                if wall_distance
+                   current_distance = wall_distance.ranges[0]
+                   distance_error = (current_distance - distance).abs
+
+                   transition! if distance_error < WALL_DISTANCE_THRESHOLD
+                end
+            end
+
+            emit :success
+        end
+
+        move_forward.depends_on alignment
+        move_forward.should_start_after alignment
+        move_forward
     end
 
     # -------------------------------------------------------------------------
