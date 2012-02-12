42f9354b08b44be493d8bce414af2d3897e24f49
Chris Mueller <christoph.mueller@dfki.de>
2012-02-12 22:51:17 +0100

    rename move to align_and_move. add source code for search_frontal_distance.

--- planners/main.rb
+++ planners/main.rb
@@ -2,19 +2,6 @@
 # to the interface planner list. For Avalon it should provides
 # global utility functions used in plannning scripting.
 class MainPlanner < Roby::Planning::Planner
-
-    # use a defined data_reader 'orientation_reader' for fetching current yaw
-    def get_current_yaw(orientation_reader)
-        yaw = nil
-        poll do
-            if o = orientation_reader
-                yaw = o.orientation.yaw
-                transition!
-            end
-        end
-        yaw
-    end
-
 end
 
 
