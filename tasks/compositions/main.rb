fcd101d40b86631bf6e4f50da47819115dbcb616
Chris Mueller <christoph.mueller@dfki.de>
2012-02-13 20:38:52 +0100

    move some data_services for controlLoop to tasks/compositions/main.rb

--- tasks/compositions/main.rb
+++ tasks/compositions/main.rb
@@ -35,6 +35,8 @@ Cmp::ControlLoop.controller_type 'Motion2D', '/base/MotionCommand2D'
 Cmp::ControlLoop.controller_type 'AUVMotion', '/base/AUVMotionCommand'
 
 using_task_library 'auv_rel_pos_controller'
+using_task_library 'avalon_control'
+using_task_library 'hbridge'
 
 composition 'VisualServoing' do
     add Srv::RelativePositionDetector, :as => 'detector'
@@ -44,4 +46,54 @@ composition 'VisualServoing' do
     autoconnect
 end
 
+# NOTE: the 'controller' => Bla part of the specialization should not be needed,
+# but is as of today (29.05.2011)
+Cmp::ControlLoop.specialize 'command' => AuvRelPosController::Task do
+#    add Srv::OrientationWithZ
 
+    overload 'controller', Srv::AUVMotionController
+    #connect command.motion_command => controller.command
+    export command.position_command
+    
+    autoconnect
+end
+
+Cmp::ControlLoop.specialize 'controller' => AvalonControl::MotionControlTask do
+    add Srv::OrientationWithZ, :as => 'pose'
+    connect pose.orientation_z_samples => controller.pose_samples
+end
+
+Cmp::ControlLoop.specialize 'controller' => AvalonControl::PositionControlTask do
+    add Srv::Pose
+    autoconnect
+end
+
+require 'roby/tasks/timeout'
+Compositions::ControlLoop.specialize 'actuators' => Hbridge::Task do
+    add Hbridge::Task, :as => 'actuators',
+        :consider_in_pending => false,
+        :failure => [:read_only.not_followed_by(:read_write), :stop]
+
+    on :start do |ev|
+        hbridge = child_from_role 'actuators'
+
+        # When we start the control composition, disable error handling for the
+        # hbridge for 10 seconds in order to wait for it to do its calibration
+        # and/or simply the read_only to read_write switch
+        timeout = Roby::Tasks::Timeout.new(:delay => 10)
+        timeout.on(:start) { |ev| Robot.info "delaying read_only errors by #{timeout.delay} seconds" }
+        timeout.on(:timed_out)  { |ev| Robot.info "timed out on read_only to read_write switch, resuming handling of hbridge errors" }
+        hbridge.read_only_event.handle_with(timeout)
+        timeout.start!
+
+      Robot.info "hb is of class #{hbridge.class}"
+
+        Robot.info  "timout started"
+        # But resume error handling as soon as read_write is emitted
+        hbridge.read_write_event.on do |event|
+            Robot.info "Event: #{event} is calles"
+            Robot.info "hbridge switched to read_write, resuming handling of hbridge errors"
+        end
+        hbridge.read_write_event.signals timeout.stop_event
+    end
+end
--- tasks/orogen/avalon_control.rb
+++ tasks/orogen/avalon_control.rb
@@ -2,13 +2,4 @@ class AvalonControl::MotionControlTask
     provides Srv::AUVMotionController
 end
 
-Cmp::ControlLoop.specialize 'controller' => AvalonControl::MotionControlTask do
-    add Srv::OrientationWithZ, :as => 'pose'
-    connect pose.orientation_z_samples => controller.pose_samples
-end
-
-Cmp::ControlLoop.specialize 'controller' => AvalonControl::PositionControlTask do
-    add Srv::Pose
-    autoconnect
-end
 
