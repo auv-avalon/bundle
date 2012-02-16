7666a4a1f00e672ab4b52a8e0233a1b3b2aa2269
Chris Mueller <christoph.mueller@dfki.de>
2012-02-16 16:35:40 +0100

    adjust wall_servoing configuration for planning.

--- controllers/simulation.rb
+++ controllers/simulation.rb
@@ -8,16 +8,16 @@ module Robot
         task.setPosition(x, y, z)
     end
 
-    def self.avalon_set_pose(x, y, z, yaw)
+    def self.sim_set_pose(x, y, z, yaw)
         task = Orocos::TaskContext.get 'avalon_simulation'
         task.setPosition(x, y, z)
         task.setYaw(yaw)
     end
 
-    def self.set_avalon_to(object)
+    def self.sim_set_avalon(object)
         buoy = { :x => 55.0, :y => -1.0, :z => -2.5, :theta => Math::PI / 2.0 }
         pipe = { :x => 0.0, :y => 0.0, :z => -0.0, :theta => 0.0 }
-        wall = { :x => 58.0, :y => 2.5, :z => -4.5, :theta => Math::PI / 2.0 }
+        wall = { :x => 45.0, :y => 2.5, :z => -4.5, :theta => 0.0 }
 
         position = nil
 
