14e8a3d5dbd8fe6c625749fa06d46754de1ff06e
deshalb <deshalb@deshalb.dfki.uni-bremen.de>
2012-05-11 13:38:12 +0200

    changed uvw_dynamic_model parameters

--- tasks/orogen/uwv_dynamic_model.rb
+++ tasks/orogen/uwv_dynamic_model.rb
@@ -36,8 +36,8 @@ class AvalonModelParameters
 
     # UWV physical parameters
     def self.vehicle_physical_parameters(task)
-        task.uwv_mass = 63 		# total mass of the vehicle in kg
-        task.uwv_volume = (2*(0.55*0.15*0.15*Math::PI)+0.30*Math::PI*0.15*0.15) # total volume of the vehicle - considered as two cylinders and center as cylinder
+        task.uwv_mass = 68 		# total mass of the vehicle in kg
+        task.uwv_volume = ((0.445+0.56)*0.13*0.13*Math::PI+0.41*Math::PI*0.18*0.18)#(2*(0.55*0.15*0.15*Math::PI)+0.30*Math::PI*0.15*0.15) # total volume of the vehicle - considered as two cylinders and center as cylinder
         task.uwv_float = true		# to assume that the vehicle floats . i.e gravity == buoyancy
     end
 
@@ -50,50 +50,50 @@ class AvalonModelParameters
     # UWV interia + added mass Matrix
     # consider Avalon as one big cylinder
     def self.mass_matrix(task)
-        task.mass_matrix = [50,0,0,0,0,0,  0,20,0,0,0,0,  0,0,30,0,0,0,  0,0,0,10,0,0,  0,0,0,0,10,0,  0,0,0,0,0,10]
+        task.mass_matrix = [7.48,0,0,0,0,0,  0,16.29,0,0,0,0,  0,0,16.29,0,0,0,  0,0,0,0.61,0,0,  0,0,0,0,1.67,0,  0,0,0,0,0,1.67]
     end
 
     # Linear damp coefficient
     def self.linDampCoeff(task)
-        task.linDampCoeff = [40.88, 10, 10, 10, 10, 10 ]
+        task.linDampCoeff = [1.2832, 10, 10, 0, 1.7, 1.7 ]
     end
 
     # Quadratic damp coefficient
     def self.quadDampCoeff(task)
-        task.quadDampCoeff = [155, 50, 50, 10, 10, 10]
+        task.quadDampCoeff = [8.962, 20, 20, 10, 50, 50]
     end
     # ThrusterCoefficient
     def self.thrusterCoefficient(task)
-        task.thruster_coefficient.surge.positive = 0.0000045
-        task.thruster_coefficient.surge.negative = 0.0000045
-        task.thruster_coefficient.sway.positive = 0.0000045
-        task.thruster_coefficient.sway.negative = 0.0000045
-        task.thruster_coefficient.heave.positive = 0.0000045
-        task.thruster_coefficient.heave.negative = 0.0000045
-        task.thruster_coefficient.roll.positive = 0.0
-        task.thruster_coefficient.roll.negative = 0.0
-        task.thruster_coefficient.pitch.positive = 0.0000045
-        task.thruster_coefficient.pitch.negative = 0.0000045
-        task.thruster_coefficient.yaw.positive = 0.0000045
-        task.thruster_coefficient.yaw.negative = 0.0000045
+        task.thruster_coefficient.surge.positive = 0.005#-0.000045#-0.0000045
+	task.thruster_coefficient.surge.negative = 0.005#-0.000045#-0.0000045
+	task.thruster_coefficient.sway.positive = 0.005#000045
+	task.thruster_coefficient.sway.negative = 0.005#000045
+	task.thruster_coefficient.heave.positive = 0.005#000045
+	task.thruster_coefficient.heave.negative = 0.005#000045
+	task.thruster_coefficient.roll.positive = 0.0
+	task.thruster_coefficient.roll.negative = 0.0
+	task.thruster_coefficient.pitch.positive = 0.0#000045
+	task.thruster_coefficient.pitch.negative = 0.0#000045
+	task.thruster_coefficient.yaw.positive = 0.0#000045
+	task.thruster_coefficient.yaw.negative = 0.0#000045
     end
 
     # Thruster mapping
     def self.thruster_mapping(task)
         array_thruster_value = task.thruster_value.to_a
-            array_thruster_value= [10 , 10, 0, 0 ,0]
+            array_thruster_value= [25.4 , 25.4, 25.4, 25.4 , 25.4, 25.4]
         task.thruster_value = array_thruster_value
 
         array_thruster_mapped_names = task.thruster_mapped_names.to_a
             #array_thruster_mapped_names= "SURGE,SURGE,SWAY,SWAY,HEAVE,HEAVE".split(/,/)
-            array_thruster_mapped_names= ['SURGE','SURGE','SWAY','HEAVE','HEAVE']
+            array_thruster_mapped_names= ['HEAVE','HEAVE','SURGE','SURGE','SWAY','SWAY']
         task.thruster_mapped_names = array_thruster_mapped_names
 
     end
 
     # Thruster control matrix
     def self.thruster_control_matrix(task)
-        task.thruster_control_matrix = [1,0,0,0,0,-0.5, 1,0,0,0,0,0.5, 0,1,0,0,0,0, 0,0,1,0,-0.5,0, 0,0,1,0,0.5,0]
+        task.thruster_control_matrix = [0,0,1,0,-0.92,0,  0,0,1,0,0.205,0,  1,0,0,0,0,-0.17,  1,0,0,0,0,0.17,  0,1,0,0,0,-0.81,  0,1,0,0,0,0.04]
     end
 
     # Simulation parameters
@@ -116,7 +116,7 @@ class AvalonModelParameters
         thruster_mapping(task.thrusters)
         thruster_control_matrix(task)
         simulation_data(task)
-        task.thrusterVoltage = 30
+        task.thrusterVoltage = 25.4
 
     end
 end
