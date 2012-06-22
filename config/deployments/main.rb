c4a67a4a12b2e08c7641956cc6ab8709fe20f710
auv <auv@avalon>
2012-06-22 20:07:41 +0200

    use OrientationEstimator for speed samples in localizations.

--- config/deployments/main.rb
+++ config/deployments/main.rb
@@ -11,9 +11,9 @@ define('drive_simple', Cmp::ControlLoop.
 
 define('uwv_dynamic_model', Cmp::UwvModel)
 define('hough_localization', Cmp::SonarWallHough.use('sonar'))
-define('particle_localization', Cmp::Localization.use('sonar').use(Cmp::UwvModel))
-define('localization', Cmp::DualLocalization.use('sonar').use(Cmp::UwvModel))
-define('navigation', Cmp::Navigation.use(Cmp::DualLocalization.use('sonar').use(Cmp::UwvModel)))
+define('particle_localization', Cmp::Localization.use('sonar').use(Cmp::OrientationEstimator))
+define('localization', Cmp::DualLocalization.use('sonar').use(Cmp::OrientationEstimator))
+define('navigation', Cmp::Navigation.use(Cmp::DualLocalization.use('sonar').use(Cmp::OrientationEstimator)))
 
 define('cross_sonar', Cmp::DualSonarWallDetector. 
        use('sonar_front' => device('sonar').use_conf('default', 'straight_front')).
