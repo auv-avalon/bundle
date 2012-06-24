3fa23d3e355672870ac2bc0df7e63ce37401e144
Allan E. Conquest <conquest@informatik.uni-bremen.de>
2012-06-24 16:45:28 +0200

    indentation

--- tasks/orogen/sonar_tritech.rb
+++ tasks/orogen/sonar_tritech.rb
@@ -1,7 +1,7 @@
 class SonarTritech::Micron
     driver_for "Dev::Micron" do
         provides Srv::SonarScanProvider
-	provides Srv::GroundDistance
+        provides Srv::GroundDistance
     end
 end
 
