5941a9c509cbcd75a35127d6c80b13349df844c6
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2014-01-28 12:46:50 +0100

    Small cleanup of auto_starter script

--- scripts/controllers/auto_starter.rb
+++ scripts/controllers/auto_starter.rb
@@ -3,10 +3,10 @@ require 'models/profiles/main'
 
 #State.soft_timeout = #something
 
-current_mode = nil 
-current_submode = nil
-run_start = nil
-last_navigation_task = nil
+State.current_mode = nil 
+State.current_submode = nil
+State.run_start = nil
+State.last_navigation_task = nil
 State.lowlevel_substate  = -1
 State.lowlevel_state = -1
 
@@ -14,46 +14,31 @@ State.lowlevel_state = -1
 #Define the possible modes that can be set
 State.navigation_mode = ["drive_simple_def","buoy_def", "pipeline_def", "wall_right_def"]
 
-#Reading the Joystick task to react on changes if an statechage should be done...
-Roby.every(0.1, :on_error => :disable) do
-    #Check wether we should stop an current operation mode
-    if (State.lowlevel_state != 5 and  State.lowlevel_state != 3) or ((State.lowlevel_substate != current_submode) and current_submode)
-        if current_mode
-            Roby.plan.unmark_mission(current_mode.task)
-            last_navigation_task = current_mode.task
-            current_mode = nil
-            current_submode = nil
-        end
-    end
-
-
-    #Workaround for someting withing roby
-    if last_navigation_task
-        last_navigation_task = nil if !last_navigation_task.plan # WORKAROUND: we're waiting for the task to be GCed by Roby before injecting the next navigation mode
-
-    #Check if we should start anything
-    elsif State.lowlevel_state == 5 or State.lowlevel_state == 3
+def check_for_switch
+    if State.lowlevel_state == 5 or State.lowlevel_state == 3
         #Make sure nothing is running so far to prevent double-starting
-        if !current_mode
+        if State.current_mode.nil?
             #Check if the submode is a valid one
             if(State.navigation_mode[State.lowlevel_substate])
-                Robot.info "starting navigation mode #{State.navigation_mode[State.lowlevel_substate]}, we are currently at #{current_mode}"
-                current_submode = State.lowlevel_substate
+                Robot.info "starting navigation mode #{State.navigation_mode[State.lowlevel_substate]}, we are currently at #{State.current_mode}"
+                State.current_submode = State.lowlevel_substate
                 nm, _ = Robot.send("#{State.navigation_mode[State.lowlevel_substate]}!")
-                pp nm
-                current_mode = nm.as_service
+                #pp nm
+                State.current_mode = nm.as_service
             elsif
                 Robot.info "Cannot Start unknown substate!!!!! -#{State.navigation_mode[State.lowlevel_substate]}-"
             end
 
             if(State.soft_timeout?)
-                run_start = Time.now
+                State.run_start = Time.now
             end
         end
     end
+end
 
-    if(State.soft_timeout? and run_start)
-        if(Time.now - run_start > State.soft_timeout)
+def check_for_mission_timeout
+    if(State.soft_timeout? and State.run_start)
+        if(Time.now - State.run_start > State.soft_timeout)
             begin
     	        Orocos::TaskContext.get('hbridge').stop
                 rescue Exception => e
@@ -62,4 +47,33 @@ Roby.every(0.1, :on_error => :disable) do
             Roby.engine.quit
         end
     end
+end
+
+#Reading the Joystick task to react on changes if an statechage should be done...
+Roby.every(0.1, :on_error => :disable) do
+
+    #Check wether we should stop an current operation mode
+    if (State.lowlevel_state != 5 and  State.lowlevel_state != 3) or ((State.lowlevel_substate != State.current_submode) and State.current_submode)
+        if State.current_mode
+            Roby.plan.unmark_mission(State.current_mode.task)
+            last_navigation_task = State.current_mode.task
+            State.current_mode = nil
+            State.current_submode = nil
+        end
+    end
+
+    safe_mode = false
+
+    #Workaround for someting withing roby
+    if safe_mode
+        if last_navigation_task
+            last_navigation_task = nil if !last_navigation_task.plan # WORKAROUND: we're waiting for the task to be GCed by Roby before injecting the next navigation mode
+        elsif 
+            check_for_switch
+        end
+    else
+        check_for_switch
+    end
+
+    check_for_mission_timeout
 end
