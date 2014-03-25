a7e53561b09b8d5e8b499c02b628d6836f49d423
auv <avalon@dfki.de>
2014-03-25 18:37:16 +0100

    Added orientation correction task

--- models/blueprints/pose_avalon.rb
+++ models/blueprints/pose_avalon.rb
@@ -2,6 +2,7 @@ using_task_library "xsens_imu"
 using_task_library "fog_kvh"
 using_task_library "orientation_estimator"
 using_task_library "depth_reader"
+using_task_library "uw_particle_localization"
 
 
 require "rock/models/blueprints/pose"
@@ -10,14 +11,19 @@ require "models/blueprints/avalon.rb"
 module PoseAvalon
     class DagonOrientationEstimator < Syskit::Composition
         add OrientationEstimator::BaseEstimator, :as => 'estimator'
-        
+        add UwParticleLocalization::OrientationCorrection, :as => 'correction'
+
         add XsensImu::Task, :as => 'imu'
         add FogKvh::Dsp3000Task, :as => 'fog'
+
+        imu_child.connect_to correction_child.absolute_orientation_port
+        estimator_child.connect_to correction_child.orientation_input_port
 
         imu_child.connect_to  estimator_child.imu_orientation_port
         fog_child.connect_to  estimator_child.fog_samples_port
 
-        export estimator_child.attitude_b_g_port, :as => 'orientation_samples'
+        #export estimator_child.attitude_b_g_port, :as => 'orientation_samples'
+        export correction_child.orientation_output_port, :as => 'orientation_samples'
         provides Base::OrientationSrv, :as => "orientation"
     end
 
