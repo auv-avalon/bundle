b38ed3cac8986acf76b123fe7d2d6285ae10e152
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2014-08-19 11:09:58 +0200

    Added test for the structure inspection

--- models/actions/main.rb
+++ models/actions/main.rb
@@ -218,6 +218,13 @@ class Main
         transition(blind2.success_event, s1)
     end
 
+    describe("foo")
+    state_machine "sim_test_structure" do
+        set = state sim_setter_def(:node => "avalon", :posX => -80, :posY => 25, :posZ => -10, :rotZ => -90)
+        detector = state structure_inspection_def
+        start set
+        transition(set.success_event, detector)
+    end
     
     describe("Do the minimal demo for the halleneroeffnung, means pipeline, then do wall-following and back to pipe-origin")
     state_machine "advanced_demo_once" do
