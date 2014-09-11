4ff87d97c9a11aa6c74b982eaff7e65b902acdc6
Your Name <you@example.com>
2014-09-11 18:55:06 +0200

    Fixed avalon thruster mappign for sim

--- models/actions/avalon_simulation/main.rb
+++ models/actions/avalon_simulation/main.rb
@@ -6,7 +6,7 @@ class Main < Roby::Actions::Interface
 
     describe("foo")
     state_machine "sim_test_structure" do
-        set = state sim_setter_def(:node => "avalon", :posX => -80, :posY => 25, :posZ => -10, :rotZ => -90)
+        set = state sim_setter_def(:node => "avalon", :posX => -50, :posY => 20, :posZ => -10, :rotZ => 90) 
         detector = state structure_inspection_def
         start set
         transition(set.success_event, detector)
